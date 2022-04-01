f1f30e8ba8ec1634c9b96715825a3a1ffcc16f54
Philippe <philippe.liogier@matera.eu>
2022-04-01 10:55:23 +0200

    add apps folder

---
+++ apps/test/craco.config.js
@@ -0,0 +1,26 @@
+
+//const CracoEsbuildPlugin = require('craco-esbuild')
+//const { ProvidePlugin } = require('webpack')
+const path = require("path");
+const { getLoader, loaderByName } = require("@craco/craco");
+
+const packages = [];
+packages.push(path.join(__dirname, "../../packages/test-common"));
+
+module.exports = {
+  webpack: {
+    configure : (webpackConfig, arg) => {
+      const { isFound, match } = getLoader(webpackConfig, loaderByName("babel-loader"));
+      if (isFound) {
+        const include = Array.isArray(match.loader.include)
+          ? match.loader.include
+          : [match.loader.include];
+  
+        match.loader.include = include.concat(packages);
+      }
+      console.log(match.loader.include)
+  
+      return webpackConfig;
+    }
+  }
+}
---
+++ apps/test/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@matera/test-common';
+
+function App() {
+  return (
+    <div className="App">
+      Button from @matera/test-common package : inside apps/test
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
