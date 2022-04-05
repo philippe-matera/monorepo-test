07dba0c784014e0e6e05c26fa0b2d1765a6ba37c
Philippe <philippe.liogier@matera.eu>
2022-04-05 17:25:18 +0200

    add react-toastify to apps/test-app

--- apps/test-app/src/App.js
+++ apps/test-app/src/App.js
@@ -1,17 +1,25 @@
 import { Button } from '@matera/test-common';
 import { COLORS, buildTranslationKeys } from '@matera-tech/utils';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const key = "toto";
   const result = buildTranslationKeys(key, ["suffix"])
+  const notify = () => toast("Wow so easy!");
+
 
   console.log(result)
   return (
     <div className="App">
-      <div>Button from @matera/test-common</div>
-      <Button/>
-      <div>Color :</div>
-      {COLORS.success.normal}
+        <div>Button from @matera/test-common</div>
+        <Button/>
+        <div>Color :</div>
+        {COLORS.success.normal}
+        <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
     </div>
   );
 }
